899947eeeabb07e84f7d20d67249e8a6bcf6191b
Erico Almeida <ericoalmeidamvp@gmail.com>
2022-07-02 15:20:04 -0400

    Add test to vallidate if stack is empty

---
+++ test/stack/based-in-object/stack-spec.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+import Stack from '../../../src/stack/based-in-object/stack';
+
+describe('Stack', () => {
+  describe('#isEmpty', () => {
+    it('Should create an empty stack', () => {
+      const stack = new Stack();
+
+      expect(stack.isEmpty()).to.be.true;
+    });
+  });
+});
