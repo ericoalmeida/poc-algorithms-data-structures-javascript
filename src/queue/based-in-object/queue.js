2f00d6e6fe90f3be60ce43eb006d21c729500919
Erico Almeida <erico.silva@invillia.com>
2022-09-01 08:40:56 -0400

    Add queue class based in object

---
+++ src/queue/based-in-object/queue.js
@@ -0,0 +1,61 @@
+export default class Queue {
+  constructor() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+
+  enqueue(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return;
+    }
+
+    const element = this.items[this.lowestCount];
+
+    delete this.items[this.lowestCount];
+    this.lowestCount++;
+
+    return element;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return;
+    }
+
+    return this.items[this.lowestCount];
+  }
+
+  size() {
+    return this.count - this.lowestCount;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  clear() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      return '';
+    }
+
+    let objString = `${this.items[this.lowestCount]}`;
+
+    for (let i = ++this.lowestCount; i < this.count; i++) {
+      objString = `${objString},${this.items[i]}`;
+    }
+
+    return objString;
+  }
+}
